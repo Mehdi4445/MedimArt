56047cc7c6a75c514201965e8005f9edb22de4ea
Mehdi Medimegh <79759194+Mehdi4445@users.noreply.github.com>
2023-07-24 11:09:56 +0100

    finished experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,13 +20,32 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Mehdi</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br className="sm:block hidden"/> sed do
-            eiusmod tempor incididunt ut <br className="sm:block hidden"/> labore et dolore magna aliqua. Sit amet
-            consectetur adipiscing elit ut.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,{" "}
+            <br className="sm:block hidden" /> sed do eiusmod tempor incididunt
+            ut <br className="sm:block hidden" /> labore et dolore magna aliqua.
+            Sit amet consectetur adipiscing elit ut.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary
+          flex justify-center items-start p-2"
+          >
+            <motion.dev 
+                animate={{ y: [0, 24, 0] }}
+                transition={{
+                          duration: 1.5,
+                          repeat: Infinity,
+                          repeatType: 'loop'
+                }}
+                className="w-3 h-3 rounded-full bg-secondary mb-1"/>
+
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from "react";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, events } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -22,8 +22,8 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.65}
-        position={[-0.5, -3.25, -1.5]}
+        scale={isMobile ? 0.38 : 0.65}
+        position={isMobile ? [-5, -1.2, -1.7] : [-0.5, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -31,6 +31,22 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -47,7 +63,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
